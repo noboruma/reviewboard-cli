45b8e787a1f6b554eede6498bad570e845b8aee7
Thomas Legris <thomas.legris@mathworks.co.uk>
2018-05-11 17:38:29 +0100

    Add comments support

--- getlastdiffs.sh
+++ getlastdiffs.sh
@@ -12,15 +12,41 @@ function download_and_patch() {
     input_newfile=$2
 
     out_file=$3
+    comments_url=$4
 
     curl -k --create-dirs $input_oldfile -o $out_file.old -sS
     curl -k --create-dirs $input_newfile -o $out_file.new -sS
+
+    ## Get and add comments
+    comments=`curl -s -k --create-dirs $comments_url -H "Accept: application/json"`
+    echo $comments
+
+    total_comments=`echo $comments | jq '.total_results'`
+    total_comments=`expr $total_comments - 1`
+    for i in $(seq 0 $total_comments); do
+        userid=`echo $comments | jq '.diff_comments['$i'].links.user.title'`
+        timestamp=`echo $comments | jq '.diff_comments['$i'].timestamp'`
+        status=`echo $comments | jq '.diff_comments['$i'].issue_status'`
+        text=`echo $comments | jq '.diff_comments['$i'].text'`
+        first_line=`echo $comments | jq '.diff_comments['$i'].first_line'`
+        num_lines=`echo $comments | jq '.diff_comments['$i'].num_lines'`
+        last_line=`expr $first_line + $num_lines`
+        formated_comment=`echo "/* $timestamp, $userid: $text */"`
+        if jq -e '.diff_comments['$i'].issue_opened' >/dev/null 2>&1 <<<"$comments"; then
+            sed -i "${first_line}i /* @@ begin @@ */" $out_file.new
+            sed -i "${first_line}i $formated_comment" $out_file.new
+            sed -i "${last_line}i /* @@ end @@ */ " $out_file.new
+        else
+            sed -i "${first_line}i $formated_comment" $out_file.new
+        fi
+    done
 
     diff -u $out_file.old $out_file.new > $out_file.patch
     echo "$out_file.patch generated"
 }
 
 rbdiff=`curl -s -k $REVIEWBOARD_URL/api/review-requests/$rbid/diffs/ -H "Accept: application/json"`
+echo $rbdiff
 last_diff=`echo $rbdiff | jq '.total_results'`
 echo "This RB contains $last_diff diffs"
 echo "Diff last diff against original"
@@ -35,13 +61,14 @@ total_files=`echo $rbdifffiles | jq '.total_results'`
 total_files=`expr $total_files - 1`
 for i in $(seq 0 $total_files); do
     fileid=`echo $rbdifffiles | jq '.files['$i'].id'`
+    comments=`echo $rbdifffiles | jq -r '.files['$i'].links.diff_comments.href'`
     original_file=`echo $rbdifffiles | jq -r '.files['$i'].links.original_file.href'`
     patched_file=`echo $rbdifffiles | jq -r '.files['$i'].links.patched_file.href'`
 
     destination_filename=`echo $rbdifffiles | jq -r '.files['$i'].dest_file'`
     destination_filename=`echo "$destination_filename" | sed "s/^.*$commonroot/$commonroot/g"`
 
-    download_and_patch $original_file $patched_file $outFolder/$destination_filename &
+    download_and_patch $original_file $patched_file $outFolder/$destination_filename $comments &
     pids="$pids $!"
 done
 
