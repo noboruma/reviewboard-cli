b1c6d61470d16fcba0c8fc0a78751b16a9a82c14
Thomas Legris <thomas.legris@mathworks.co.uk>
2018-05-14 13:45:23 +0100

    Add comments support

--- getlastdiffs.sh
+++ getlastdiffs.sh
@@ -19,10 +19,10 @@ function download_and_patch() {
 
     ## Get and add comments
     comments=`curl -s -k --create-dirs $comments_url -H "Accept: application/json"`
-    echo $comments
 
     total_comments=`echo $comments | jq '.total_results'`
     total_comments=`expr $total_comments - 1`
+    cp $out_file.new $out_file.new.ref
     for i in $(seq 0 $total_comments); do
         userid=`echo $comments | jq '.diff_comments['$i'].links.user.title'`
         timestamp=`echo $comments | jq '.diff_comments['$i'].timestamp'`
@@ -30,23 +30,34 @@ function download_and_patch() {
         text=`echo $comments | jq '.diff_comments['$i'].text'`
         first_line=`echo $comments | jq '.diff_comments['$i'].first_line'`
         num_lines=`echo $comments | jq '.diff_comments['$i'].num_lines'`
+        # Add the comment line
         last_line=`expr $first_line + $num_lines`
         formated_comment=`echo "/* $timestamp, $userid: $text */"`
-        if jq -e '.diff_comments['$i'].issue_opened' >/dev/null 2>&1 <<<"$comments"; then
-            sed -i "${first_line}i /* @@ begin @@ */" $out_file.new
-            sed -i "${first_line}i $formated_comment" $out_file.new
-            sed -i "${last_line}i /* @@ end @@ */ " $out_file.new
+        cp $out_file.new $out_file.tmp.$i
+        if jq -e '.diff_comments['$i'].extra_data.severity' >/dev/null 2>&1 <<<"$comments"; then
+            sed -i "${last_line}i /* @@ end @@ */ " $out_file.tmp.$i
+            sed -i "${first_line}i /* @@ begin @@ */" $out_file.tmp.$i
+            sed -i "${first_line}i $formated_comment" $out_file.tmp.$i
         else
-            sed -i "${first_line}i $formated_comment" $out_file.new
+            sed -i "${first_line}i $formated_comment" $out_file.tmp.$i
         fi
     done
+    for i in $(seq 0 $total_comments); do
+        merge $out_file.new $out_file.new.ref $out_file.tmp.$i
+        rm $out_file.tmp.$i
+    done
+    rm $out_file.new.ref
+
+    #Remove all conflicts and consider them as adds
+    sed -i '/>>>>>>.*/d' $out_file.new
+    sed -i '/<<<<<<.*/d' $out_file.new
+    sed -i '/======.*/d' $out_file.new
 
     diff -u $out_file.old $out_file.new > $out_file.patch
     echo "$out_file.patch generated"
 }
 
 rbdiff=`curl -s -k $REVIEWBOARD_URL/api/review-requests/$rbid/diffs/ -H "Accept: application/json"`
-echo $rbdiff
 last_diff=`echo $rbdiff | jq '.total_results'`
 echo "This RB contains $last_diff diffs"
 echo "Diff last diff against original"
